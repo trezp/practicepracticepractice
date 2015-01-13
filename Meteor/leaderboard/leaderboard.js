fd96e6b84a4afd7a799135b0529fba57eeae0603
Treasure Porth <treasure.rose@gmail.com>
2015-01-12 21:12:34 -0800

    added user accounts

--- Meteor/leaderboard/leaderboard.js
+++ Meteor/leaderboard/leaderboard.js
@@ -6,7 +6,8 @@ if(Meteor.isClient){
   
 Template.leaderboard.helpers({
   'player' : function() {
-    return PlayersList.find({}, {sort: {score: -1, name: 1}})
+    var currentUserId = Meteor.userId();
+    return PlayersList.find({createdBy: currentUserId}, {sort: {score: -1, name: 1}})
   },
   'selectedClass' : function(){
     var playerId = this._id;
@@ -35,18 +36,37 @@ Template.leaderboard.helpers({
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
       PlayersList.update(selectedPlayer, {$inc: {score: -5} });
-    }
+    }, 
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      var answer = confirm("Are you sure you want to delete this player?")
+      if(answer == true){
+        PlayersList.remove(selectedPlayer);
+      }},
+    
   });
+
   Template.addPlayerForm.events({
     'submit form': function(event){
       event.preventDefault();
       var playerNameVar = event.target.playerName.value; 
+      var currentUserId = Meteor.userId();
       PlayersList.insert({
         name: playerNameVar,
-        score: 0
-      });
-    }
-  })
+        score: 0,
+        createdBy: currentUserId
+  });
+      event.target.playerName.value = "";
+    
+      },
+      //Add the ability for to add a score upon creation of players
+    //   'click .addScore': function(event){
+    //   event.preventDefault();
+    //   var selectedPlayer = Session.get('selectedPlayer');
+    //   var addScore = event.target.addScore.value; 
+    //   PlayerList.update(selectedPlayer, {$inc: {score: addScore}});
+    // }
+    });
   
 
 };
